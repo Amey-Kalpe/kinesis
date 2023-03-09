7a967cbcc396193de9fbab662b4147525007efd3
amey.kalpe <ameykalpe16@gmail.com>
2023-03-09 22:50:05 +0530

    added send_kinesis and main methods, return 100 records from data

--- load_kinesis.py
+++ load_kinesis.py
@@ -12,7 +12,7 @@ def create_client(service, region):
 # function to load data from csv
 def load_data(filename):
     df = pd.read_csv(filename)
-    return df
+    return df.iloc[0:100]
 
 
 # functions to correctly display numbers in two value format (i.e 06 instead of 6)
@@ -45,5 +45,85 @@ def modify_date(data):
     return data
 
 
-def send_kinesis(client, stream_name, shard_count, data):
-    pass
+def send_kinesis(client, kinesis_stream_name, kinesis_shard_count, data: pd.DataFrame):
+    kinesis_records = []
+
+    rows, _ = data.shape
+
+    current_bytes = 0
+
+    row_count = 0
+
+    total_row_count = rows
+
+    send_kinesis = False
+
+    shard_count = 1
+
+    for _, row in data.iterrows():
+        values = "|".join([str(value) for value in row])
+
+        encoded_values = bytes(values, "utf-8")
+
+        kinesis_record = {"Data": encoded_values, "PartitionKey": str(shard_count)}
+
+        kinesis_records.append(kinesis_record)
+
+        row_bytes = len(values.encode("utf-8"))
+        current_bytes += row_bytes
+
+        if (
+            len(kinesis_records) == 500
+            or current_bytes > 50000
+            or row_count == total_row_count - 1
+        ):
+            send_kinesis = True
+
+        if send_kinesis:
+            client.put_records(Records=kinesis_records, StreamName=kinesis_stream_name)
+
+            kinesis_records = []
+            current_bytes = 0
+            send_kinesis = False
+
+            shard_count += 1
+
+            if shard_count > kinesis_shard_count:
+                shard_count = 1
+
+        row_count += 1
+
+    # log out how many records were pushed
+    print("Total Records sent to Kinesis: {0}".format(total_row_count))
+
+
+# main function
+def main():
+    # start timer
+    start = time.time()
+
+    # create a client with kinesis
+    kinesis = create_client("kinesis", "us-east-2")
+
+    # load in data from the csv
+    data = load_data("Sacramento_Crime_Data_From_Two_Years_Ago.csv")
+
+    # modify the date and add loadtime field
+    data = modify_date(data)
+
+    # send it to kinesis data stream
+    stream_name = "kinesis-test"
+    stream_shard_count = 1
+
+    send_kinesis(kinesis, stream_name, stream_shard_count, data)  # send it!
+
+    # end timer
+    end = time.time()
+
+    # log time
+    print("Runtime: " + str(end - start))
+
+
+if __name__ == "__main__":
+    # run main
+    main()
